45b1234bfefe263df6249ca0e8c7370d6b9c55b5
unknown <alejandrojzeballos@gmail.com>
2021-06-08 20:09:31 -0400

    {add} instalacion virtual box y windows server

--- apuntes.js
+++ apuntes.js
@@ -7,4 +7,17 @@
     Enterprise edition esta en linux y especificamente para windows server
 
     En windows server no debemos pagar por el soporte en comunity
+
+    para instlar vamos a usar la virtualizacion
+    windows 10 pro o enterprise usaremos hyper v o podemos usar virtual box en caso tengamos windows home, linux o mac.
+    requisitos hardware
+        - Debe soportar la virtualizacion
+        - equipos de 2009
+        - verificar en la bios intel vt
+    requisitos software
+        - virtual box descargar de la pagina
+            https://www.virtualbox.org/wiki/Downloads
+            usaremos la version 6.1
+        - iSO windows server 2016 o 2019
+            
 */
